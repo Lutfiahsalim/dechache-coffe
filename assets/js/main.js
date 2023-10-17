48e46c17140146b56b4c77983e65040bf2967d25
Lutfiahsalim <Lutfiah.salim12@gmail.com>
2023-10-17 09:46:35 +0800

    tampilan tablet

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,34 +31,52 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-$(document).ready(function() {
-  // Mobile nav toggle
-  $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('#navbar').toggleClass('navbar-mobile');
-      $(this).toggleClass('fa-bars fa-xmark');
-  });
+// Dapatkan elemen tombol ikon bars
+var mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 
-  // Mobile nav dropdowns activate
-  $(document).on('click', '.navbar .dropdown > a', function(e) {
-      if ($('#navbar').hasClass('navbar-mobile')) {
-          e.preventDefault();
-          $(this).next().toggleClass('dropdown-active');
-      }
-  });
+// Dapatkan elemen daftar navigasi mobile
+var navbarMobile = document.querySelector('.navbar-mobile');
 
-  // Scroll with offset on links with a class name .scrollto
-  $(document).on('click', '.scrollto', function(e) {
-      if ($(this.hash).length) {
-          e.preventDefault();
-
-          let navbar = $('#navbar');
-          if (navbar.hasClass('navbar-mobile')) {
-              navbar.removeClass('navbar-mobile');
-              let navbarToggle = $('.mobile-nav-toggle');
-              navbarToggle.toggleClass('fa-bars fa-xmark');
-          }
-          // Assuming scrollto is a function defined elsewhere in your code
-          scrollto(this.hash);
-      }
-  });
+// Tambahkan event listener untuk mengganti tampilan daftar saat tombol diklik
+mobileNavToggle.addEventListener('click', function() {
+  // Periksa apakah daftar sedang ditampilkan atau disembunyikan
+  if (navbarMobile.classList.contains('active')) {
+    // Jika ditampilkan, sembunyikan daftarnya
+    navbarMobile.classList.remove('active');
+  } else {
+    // Jika disembunyikan, tampilkan daftarnya
+    navbarMobile.classList.add('active');
+  }
 });
+
+// $(document).ready(function() {
+//   // Mobile nav toggle
+//   $(document).on('click', '.mobile-nav-toggle', function(e) {
+//       $('#navbar').toggleClass('navbar-mobile');
+//       $(this).toggleClass('fa-bars fa-xmark');
+//   });
+
+//   // Mobile nav dropdowns activate
+//   $(document).on('click', '.navbar .dropdown > a', function(e) {
+//       if ($('#navbar').hasClass('navbar-mobile')) {
+//           e.preventDefault();
+//           $(this).next().toggleClass('dropdown-active');
+//       }
+//   });
+
+//   // Scroll with offset on links with a class name .scrollto
+//   $(document).on('click', '.scrollto', function(e) {
+//       if ($(this.hash).length) {
+//           e.preventDefault();
+
+//           let navbar = $('#navbar');
+//           if (navbar.hasClass('navbar-mobile')) {
+//               navbar.removeClass('navbar-mobile');
+//               let navbarToggle = $('.mobile-nav-toggle');
+//               navbarToggle.toggleClass('fa-bars fa-xmark');
+//           }
+//           // Assuming scrollto is a function defined elsewhere in your code
+//           scrollto(this.hash);
+//       }
+//   });
+// });
