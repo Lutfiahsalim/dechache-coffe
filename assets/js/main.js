3dcac46bf55a10290e5e81e44c588dd82aff7c5e
Lutfiahsalim <Lutfiah.salim12@gmail.com>
2023-10-20 03:11:57 +0800

    semoga aman

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-// $(document).ready(function(){
-//     $('#myCarousel').carousel();
-// });
-
-//navbar
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
-
 //back to top
 // Get the button
 let mybutton = document.getElementById("myBtn");
@@ -30,46 +18,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-//navbar
-// $(document).ready(function() {
-//   // Mobile nav toggle
-//   $(document).on('click', '.mobile-nav-toggle', function(e) {
-//     console.log("Hamburger clicked!"); 
-//     var navbar = $('#navbar');
-//     navbar.toggleClass('navbar-mobile');
-//     if (navbar.hasClass('navbar-mobile')) {
-//         navbar.find('ul').show();
-//     } else {
-//         navbar.find('ul').hide();
-//     }
-//     $(this).toggleClass('fa-bars fa-xmark');
-// });
-
-
-//   // Mobile nav dropdowns activate.
-//   $(document).on('click', '.navbar .dropdown > a', function(e) {
-//       if ($('#navbar').hasClass('navbar-mobile')) {
-//           e.preventDefault();
-//           $(this).next().toggleClass('dropdown-active');
-//       }
-//   });
-
-//   // Scroll with offset on links with a class name .scrollto
-//   $(document).on('click', '.scrollto', function(e) {
-//       if ($(this.hash).length) {
-//           e.preventDefault();
-
-//           let navbar = $('#navbar');
-//           if (navbar.hasClass('navbar-mobile')) {
-//               navbar.removeClass('navbar-mobile');
-//               let navbarToggle = $('mobile-nav-toggle');
-//               navbarToggle.toggleClass('fa-bars fa-xmark');
-//           }
-//           // Assuming scrollto is a function defined elsewhere in your code
-//           scrollto(this.hash);
-//       }
-//   });
-// });
-
-//-------------------------------------------------------------
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,82 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-// const link = document.querySelector('nav ul a');
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuToggle = document.querySelector('.menu-toggle input');
+//     const nav = document.querySelector('nav ul');
 
-menuToggle.addEventListener('click', function() {
-    nav.classList.toggle('slide');
+//     menuToggle.addEventListener('click', function() {
+//         nav.classList.toggle('slide');
+//     });
+
+//     // Tambahkan event listener untuk setiap elemen menu
+//     const menuItems = document.querySelectorAll('nav ul li a');
+//     for (let i = 0; i < menuItems.length; i++) {
+//         menuItems[i].addEventListener('click', function() {
+//             // Tutup menu mobile setelah elemen <a> diklik
+//             nav.classList.remove('slide');
+//         });
+//     }
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector('.menu-toggle input');
+    const nav = document.querySelector('nav ul');
+    let isMenuOpen = false;
+    
+    menuToggle.addEventListener('click', function() {
+        toggleMenu();
+    });
+
+    // Fungsi untuk menutup menu
+    function closeMenu() {
+        nav.classList.remove('slide');
+        isMenuOpen = false;
+        // Mengganti ikon tombol toggle ketika menu ditutup
+        const spans = document.querySelectorAll('.menu-toggle input');
+        spans.forEach(span => span.classList.remove('active'));
+    }
+
+    // Tambahkan event listener pada scroll window
+    window.addEventListener('scroll', function() {
+        // Periksa posisi scroll
+        if (window.scrollY > 40 && isMenuOpen) {
+            // Jika layar di-scroll ke atas dan menu terbuka, tutup menu
+            closeMenu();
+        }
+    });
+
+    // Tambahkan event listener untuk setiap elemen menu
+    const menuItems = document.querySelectorAll('nav ul li a');
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', function() {
+            if (isMenuOpen) {
+                // Tutup menu mobile setelah elemen <a> diklik
+                closeMenu();
+            }
+        });
+    }
+
+    // Fungsi untuk toggle menu
+    function toggleMenu() {
+        nav.classList.toggle('slide');
+        isMenuOpen = !isMenuOpen;
+        // Mengganti ikon tombol toggle setelah mengkliknya
+        const spans = document.querySelectorAll('.menu-toggle input');
+        spans.forEach(span => span.classList.toggle('active'));
+    }
 });
 
 
+// SCROLLED NAV
+// Dapatkan elemen navbar Anda
+const navbar = document.querySelector('nav');
+
+// Fungsi untuk mengubah kelas saat di-scroll
+function toggleNavbarBackground() {
+    if (window.scrollY > 10) { // Anda dapat menyesuaikan offset scroll yang diperlukan di sini
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+}
+
+// Tambahkan event listener untuk mengaktifkan fungsi di atas saat di-scroll
+window.addEventListener('scroll', toggleNavbarBackground);
